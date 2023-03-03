752084a7c843c1c23f1dd1e138e15ceed2b00320
KilianBre <87305837+KilianBre@users.noreply.github.com>
2023-03-03 16:45:36 +0100

    commit

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -10,7 +10,7 @@ function reveal() {
     for (let i = 0; i < reveals.length; i++) {
         const windowHeight = window.innerHeight;
         const elementTop = reveals[i].getBoundingClientRect().top;
-        const elementVisible = 300;
+        const elementVisible = 100;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
